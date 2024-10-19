5ea8644faab4561ac86eb3206cda243cc7f3b5a2
junaietfaruk <junaietfaruk@throughltd.com>
2024-10-19 21:36:34 +0600

    project setup banner design

---
+++ src/components/Pages/Banner/Banner.js
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import banner from '../../../assests/Banner.png'
+
+const Banner = () => {
+    return (
+        <div>
+            <div className="w-full h-[900px] bg-[#07332f] ">
+                <div className="flex justify-evenly pt-32">
+                    <div className="w-[650px] space-y-12 pt-32">
+                        <h1 className="text-white text-7xl font-medium">Your Best Medical Help Center</h1>
+                        <p className="text-white">Lorem Ipsum is simply dummy text they are printing typesetting has <br /> been the industry’s stardard.</p>
+                        <button>
+                            <a href="#_" class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group">
+                                <span class="w-48 h-48 rounded rotate-[-40deg] bg-[#F7A582] absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+                                <span class="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">All Service</span>
+                            </a>
+                        </button>
+                    </div>
+                    <div>
+                        <Image src={banner} alt="banner"/>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/components/Shared/Navbar.js
@@ -0,0 +1,62 @@
+"use client";
+import { useState, useRef, useEffect } from 'react';
+import logo from '../../assests/Logo.png';
+import Image from 'next/image';
+
+const NavBar = () => {
+    const [dropDownState, setDropDownState] = useState(false);
+    const dropDownMenuRef = useRef();
+
+    useEffect(() => {
+        const closeDropDown = (e) => {
+            if (!dropDownMenuRef?.current?.contains(e?.target)) {
+                setDropDownState(false);
+            }
+        };
+
+        document.addEventListener('mousedown', closeDropDown);
+
+        return () => {
+            document.removeEventListener('mousedown', closeDropDown);
+        };
+    }, []);
+
+    return (
+        <nav className="max-w-screen-2xl mx-auto fixed top-0 left-0 right-0 flex items-center justify-between  px-4 py-2 text-white z-50">
+            <div className="scale-100 cursor-pointer rounded-2xl px-3 py-2 text-xl font-semibold text-white transition-all duration-200 hover:scale-110">
+                <Image src={logo} alt='logo' className='w-32' />
+            </div>
+            <ul className="hidden items-center justify-between gap-10 md:flex">
+                <li className="group flex cursor-pointer flex-col">
+                    Home<span className="mt-[2px] h-[3px] w-[0px] rounded-full bg-white transition-all duration-300 group-hover:w-full"></span>
+                </li>
+                <li className="group flex cursor-pointer flex-col">
+                    About<span className="mt-[2px] h-[3px] w-[0px] rounded-full bg-white transition-all duration-300 group-hover:w-full"></span>
+                </li>
+                <li className="group flex cursor-pointer flex-col">
+                    Appointment<span className="mt-[2px] h-[3px] w-[0px] rounded-full bg-white transition-all duration-300 group-hover:w-full"></span>
+                </li>
+                <li className="group flex cursor-pointer flex-col">
+                    Login<span className="mt-[2px] h-[3px] w-[0px] rounded-full bg-white transition-all duration-300 group-hover:w-full"></span>
+                </li>
+            </ul>
+            <div ref={dropDownMenuRef} onClick={() => setDropDownState(!dropDownState)} className="relative flex transition-transform md:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="cursor-pointer">
+                    <line x1="4" x2="20" y1="12" y2="12" />
+                    <line x1="4" x2="20" y1="6" y2="6" />
+                    <line x1="4" x2="20" y1="18" y2="18" />
+                </svg>
+                {dropDownState && (
+                    <ul className="z-10 gap-2 bg-[#393E46] absolute right-0 top-11 flex w-[200px] flex-col rounded-lg text-base">
+                        <li className="cursor-pointer px-6 py-2 text-white rounded-t-lg hover:bg-sky-600">Home</li>
+                        <li className="cursor-pointer px-6 py-2 text-white hover:bg-sky-600">Services</li>
+                        <li className="cursor-pointer px-6 py-2 text-white hover:bg-sky-600">About</li>
+                        <li className="cursor-pointer px-6 py-2 text-white hover:bg-sky-600">Contact</li>
+                    </ul>
+                )}
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
